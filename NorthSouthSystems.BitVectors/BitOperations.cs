5b399c1e2422bd01018a569336bf498a80671bf5
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2024-09-04 18:50:26 -0400

    Implemented a faster PopCount ulong after a greater understanding of the algorithm.

--- NorthSouthSystems.BitVectors/BitOperations.cs
+++ NorthSouthSystems.BitVectors/BitOperations.cs
@@ -6,16 +6,29 @@ namespace NorthSouthSystems.BitVectors;
 /// </summary>
 public static class BitOperations
 {
-    public static int PopCount(ulong value) =>
-          PopCount((uint)((value >> 32) & uint.MaxValue))
-        + PopCount((uint)(value & uint.MaxValue));
+    /// <summary>
+    /// Compute the number of bits set to 1 for an unsigned long value.
+    /// </summary>
+    /// <remarks>
+    /// See PopCount(uint) remarks.
+    /// </remarks>
+    public static int PopCount(ulong value)
+    {
+        value -= (value >> 1) & 0x5555_5555_5555_5555ul;
+        value = ((value >> 2) & 0x3333_3333_3333_3333ul) + (value & 0x3333_3333_3333_3333ul);
+        value = ((value >> 4) + value) & 0x0F0F_0F0F_0F0F_0F0Ful;
+        value += value >> 8;
+        value += value >> 16;
+        value += value >> 32;
+        return (int)(value & 0x0000_0000_0000_007Ful);
+    }
 
     /// <summary>
     /// Compute the number of bits set to 1 for an unsigned integer value.
     /// </summary>
     /// <remarks>
-    /// Originally found at <a href="http://www.hackersdelight.org/HDcode/newCode/pop_arrayHS.c.txt">Hacker's Delight</a>.
-    /// Found again at <a href="https://andrewlock.net/counting-the-leading-zeroes-in-a-binary-number/">Andrew Lock's Blog</a>
+    /// Originally found at http://www.hackersdelight.org, which appears to have fallen into someone else's possesion.
+    /// Found more recently at <a href="https://andrewlock.net/counting-the-leading-zeroes-in-a-binary-number/">Andrew Lock's Blog</a>
     /// when searching for a LeadingZeroCount polyfill.
     /// </remarks>
     public static int PopCount(uint value)
